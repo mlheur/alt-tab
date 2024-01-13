bfc0ac32fa88c1e8b3dfd86103ac4516da5fc809
mlheur <mlheur@gmail.com>
2024-01-13 19:15:17 +0000

    first working release

---
+++ README.md
@@ -0,0 +1,3 @@
+# Alt Tab
+
+Extension for Fantasy Grounds that allows users to switch between open windows.
---
+++ scripts/AltTab.lua
@@ -0,0 +1,50 @@
+DEBUG = true;
+
+function dbg(...)
+    if AltTab.DEBUG then
+        Debug.console("[AltTab] "..unpack(arg))
+    end
+end
+
+function CommOut(...)
+    MsgData = {}
+    MsgData["text"] = unpack(arg)
+    Comm.addChatMessage(MsgData)
+end
+
+function AltTabHandler(sCommands, sParams)
+    AltTab.dbg("++AltTabHandler( sCommands=["..sCommands.."], sParams=["..sParams.."] )");
+    nParam = tonumber(sParams)
+    if (nParam == nil) then nParam = 1 end
+    AltTab.dbg("  nParam = ["..nParam.."]")
+    WndList = Interface.getWindows()
+    if (nParam == 0) then
+        AltTab.CommOut("=== LISTING WINDOWS ===")
+        AltTab.CommOut("[id] [title] [class]")
+        AltTab.CommOut("=======================")
+        for i,wnd in pairs(WndList) do
+            wndclass = wnd.getClass()
+            if wndclass == "desktopdecal" then return end
+            wndnode = wnd.getDatabaseNode()
+            wndtitle = wndnode.getName()
+            if wndnode ~= nil then
+                wndtitlenode = wndnode.getChild("name")
+                if wndtitlenode ~= nil then
+                    wndtitle = wndtitlenode.getValue()
+                end
+            end
+            AltTab.CommOut("["..i.."] ["..wndtitle.."] ["..wndclass.."]")
+        end
+    else
+        wndclass = WndList[nParam].getClass()
+        AltTab.dbg("  calling WndList["..nParam.."].bringToFront, class=["..wndclass.."]")
+        WndList[nParam].bringToFront()
+    end
+    AltTab.dbg("--AltTabHandler(): last");
+end
+
+function onInit()
+    AltTab.dbg("++AltTab::OnInit()")
+    Comm.registerSlashHandler("alttab",AltTabHandler);
+    AltTab.dbg("--AltTab::OnInit(): last");
+end
